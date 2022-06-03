c722212a2d076703f5f6db0a91cd357febb565eb
_Maxx <93737616+m1crocrypto@users.noreply.github.com>
2022-06-04 00:22:48 +0400

    Update subspace3.sh

--- subspace3.sh
+++ subspace3.sh
@@ -32,7 +32,7 @@ cd $HOME
 echo "Nodename: '$NICKNAME', Wallet: '$SUBSPACE_ADDRESS', Disk space: '$SPACE'"
 
 # Обновление дистрибутива
-sudo apt-get update && sudo apt-get upgrade -y
+
 
 # Зависимости
 sudo apt-get install wget jq -y
